e792d4faab08dddf16f2301c3f970507cf3b2989
Itai Nathaniel <itainathaniel@gmail.com>
2019-03-22 23:33:36 +0200

    disabled API routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,11 +35,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        $this->mapApiRoutes();
+        // $this->mapApiRoutes();
 
         $this->mapWebRoutes();
-
-        //
     }
 
     /**
