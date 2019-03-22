f3eaf3dd3aeda0ceaefc18fdf17103fb872351bd
Itai Nathaniel <itainathaniel@gmail.com>
2019-03-22 23:34:36 +0200

    added /zom

---
+++ resources/views/static/zom.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Zom&reg; Browser</title>
+	<link rel="apple-touch-icon" sizes="57x57" href="zom/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="zom/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="zom/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="zom/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="zom/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="zom/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="zom/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="zom/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="zom/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="zom/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="zom/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="zom/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="zom/favicon-16x16.png">
+	<link rel="manifest" href="zom/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
+	<link rel="stylesheet" href="zom/style.css">
+</head>
+<body>
+
+<div id="zom">
+  <header>
+    <div class="container">
+      <ul>
+        <li>
+          <img src="http://it.ai/zom/zom.png" alt="Zom browser" title="Zom browser">
+        </li>
+        <li class="title">Zom&reg; Browser</li>
+      </ul>
+    </div>
+  </header>
+  
+  <div id="main">
+    <div class="container">
+      <div class="alert alert-danger">
+        Sorry, page available only with Zom&reg; Browser.
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            &copy; 2017, Zom Inc.
+          </li>
+          <li>
+            <a href="https://twitter.com/raavtan">Contact</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/itainathaniel">Terms of Service &amp; Privacy Policy</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+</div>
+
+</body>
+</html>
