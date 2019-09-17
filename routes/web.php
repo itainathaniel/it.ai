60e230b74d933aa2af70870139a67cbce351452f
Itai Nathaniel <itainathaniel@gmail.com>
2019-09-17 09:53:33 +0300

    ilil's birthday wishlist

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@
 Route::view('/', 'index');
 Route::view('/zom', 'static.zom');
 Route::view('/pikadon', 'static.pikadon');
+// Route::get('blog', 'BlogController@index');
+Route::redirect('ilil1yo', 'https://docs.google.com/document/d/1GUSXpDNdcd3QqX_nY3Bx97IBBhh4O_cDmp7QIQz8z9E/edit');
